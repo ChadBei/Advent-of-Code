958b2d40a8d1a081e879629a0efb37266e75b85a
Chad Beibide <cbeibide@bellsouth.net>
2025-12-09 15:37:52 -0600

    Implemented brute force solve, but it works

--- Day1_b/Day2.java
+++ Day1_b/Day2.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ public class Day2 {
     public static void main(String[] args) {
         System.out.println("The dial starts by pointing at 50.");
 
-        String filePath = "input.txt";
+        String filePath = "Day1/input.txt";
         String[] fileContent = FileReaderToArray.readFileToArray(filePath);
         File myFile = new File(filePath);
         System.out.println(myFile.getAbsolutePath());
@@ -18,25 +17,46 @@ public class Day2 {
         int passcode = 0;
     
         for (String line : fileContent) {
+            ////// Commenting out original method, switching to brute force count
+            // char direction = line.charAt(0);
+            // int turnAmt = Integer.parseInt(line.substring(1));
+            // int startPos = myDial.currentPosition;
+            // int endPos = myDial.turnDial(direction, turnAmt%100);
             
+            
+            // if (endPos == 0) {
+            //     passcode++;
+            // }
+
+            // passcode += turnAmt/100;
+
+            // System.out.print("The dial is rotated " + line + " to point at " + endPos);
+            // if(direction == 'R')
+            //     if ((endPos < startPos) && (endPos != 0)){
+            //         passcode++;
+            //     }
+
+            // if(direction == 'L'){
+            //     if ((endPos > startPos)&& (startPos != 0)){
+            //         passcode++;
+            //     } else if ((endPos < startPos)){
+            //         passcode++;
+            //     }
+            // }
+
+            // System.out.println(".");
+
+
+
+
+
+            ////// Bruteforce Method below
+
             char direction = line.charAt(0);
             int turnAmt = Integer.parseInt(line.substring(1));
-            int startPos = myDial.currentPosition;
-            int endPos = myDial.turnDial(direction, turnAmt%100);
-            
-            passcode += turnAmt/100;
+            passcode += myDial.bruteTurnDial(direction, turnAmt);
 
-            System.out.print("The dial is rotated " + line + " to point at " + endPos);
-            if(direction == 'R')
-                if (endPos < startPos)
-                    passcode++;
 
-            if(direction == 'L')
-                if (endPos > startPos)
-                    passcode++;
-
-            System.out.print(".");
-              
         }
 
         System.out.println("The passcode is: " + passcode);
@@ -69,6 +89,7 @@ public class Day2 {
     public static class Dial {
         public int currentPosition;
 
+        // Returns new position after 
         public int turnDial(char direction, int turnAmt){
             if(direction == 'L'){
                 currentPosition -= turnAmt;
@@ -76,18 +97,51 @@ public class Day2 {
                     currentPosition += 100;
                 }
                 return currentPosition;
-            } else if(direction == 'R'){
+            } else{
                 currentPosition += turnAmt;
                 if(currentPosition > 99){
                     currentPosition -= 100;
                 }
                 return currentPosition;
             }
-            return -1;
+        }
+
+        public int bruteTurnDial(char direction, int turnAmt){
+            int numZero = 0;
+            int position = currentPosition;
+
+            // For loop will move dial 1 click each iteration
+            for(int i= turnAmt; i > 0; i--){
+                position = bruteTurnOne(direction, position);
+
+                if (position == 0)
+                    numZero++;
+            }
+            currentPosition = position;
+
+            return numZero;
+
+        }
+
+        private int bruteTurnOne(char direction, int current){
+
+            if (direction == 'L') 
+                current--;
+
+            if (direction == 'R')
+                current++;
+
+            if (current > 99)
+                return 0;
+
+            if (current < 0)
+                return 99; 
+
+            return current;
         }
 
         public Dial(){
-            currentPosition = 50;
+            this.currentPosition = 50;
         }
     }
 
