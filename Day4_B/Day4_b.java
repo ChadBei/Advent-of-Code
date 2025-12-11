f2ab89520795c3fce333a7d9b430e9b2c6c15f0e
Chad Beibide <cbeibide@bellsouth.net>
2025-12-11 11:28:56 -0600

    finished 4_b

---
+++ Day4_B/Day4_b.java
@@ -0,0 +1,100 @@
+package Day4_B;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day4_b {
+     public static void main(String[] args) throws FileNotFoundException {
+
+        //Read input into 2d char array
+        String filePath = "Day4_a/input.txt";
+        ArrayList<String> lines = new ArrayList<>();
+        int numLines=0;
+
+        File file = new File(filePath);
+        Scanner scanner = new Scanner(file);
+
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
+            numLines++;
+        }
+        scanner.close();
+
+        char[][] paperMap = new char[lines.get(0).length()][numLines];
+
+        for(int i = 0;i<lines.get(0).length(); i++){
+            for(int j = 0; j<numLines;j++){
+                paperMap[i][j]=lines.get(i).charAt(j);
+                // System.out.print(lines.get(i).charAt(j));
+            }
+            // System.out.println();
+        }
+
+        int passcode = 0;
+        boolean madeChange;
+
+        
+            do {
+                madeChange = false;
+                for(int i = 0; i<paperMap[0].length; i++){
+                    for(int j = 0; j<numLines; j++){
+                        if(paperMap[i][j] == 'X'){
+                            paperMap[i][j] = '.';
+                        }
+                    }
+                }
+                for (int i = 0; i < paperMap[0].length; i++) {
+                    for (int j = 0; j < numLines; j++) {
+
+                        // Skip char if not paper
+                        if(paperMap[i][j] != '@'){
+                            continue;
+                        }
+
+                        int touchCount =0;    
+
+                        // Itendify all edges
+                        int rStart=-1;
+                        int cStart=-1;
+                        int rEnd=1;
+                        int cEnd=1;
+        
+
+                        if(i == 0)
+                            rStart=0;
+                        if(i == paperMap[i].length-1)
+                            rEnd=0;
+                        if(j==0)
+                            cStart=0;
+                        if(j == numLines-1)
+                            cEnd=0;    
+
+
+                        for(int r = rStart; r<=rEnd;r++){
+                                for(int c = cStart; c<=cEnd; c++){
+                                    if(paperMap[i+r][j+c] == '@' || paperMap[i+r][j+c] == 'X'){
+                                        touchCount++;
+                                    }
+                                    if(r==0 && c==0){
+                                        touchCount--;
+                                    }
+                                            
+                                }
+                            }
+
+                        if(touchCount < 4){
+                            passcode++;
+                            paperMap[i][j] = 'X';
+                            madeChange = true;
+                        }
+                    }    
+                }
+            } while(madeChange);
+
+            System.err.println("Passcode is " + passcode);
+        }
+
+    }
+
